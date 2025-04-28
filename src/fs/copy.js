9370f84790ba9d3df85827cfd4298075026480d0
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-04-28 23:28:23 +0300

    fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import { cp, access } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const _dirname = dirname(fileURLToPath(import.meta.url))
+        const sourcePath = join(_dirname, 'files')
+        const destinationPath = join(_dirname, 'files_copy')
+
+        try {
+            await access(destinationPath)
+            throw new Error('FS operation failed')
+        } catch (error) {
+            if (error.code !== 'ENOENT') throw error;
+        }
+
+        await cp(sourcePath, destinationPath, {
+            recursive: true,
+            force: false,
+            errorOnExist: true,
+        })
+    } catch (error) {
+        throw error
+    }
 };
 
 await copy();
