91e90be217078d22cc1827f66abf8177beadd2df
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-04-28 23:47:47 +0300

    fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { access, readdir } from 'fs/promises';
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const _dirname = dirname(fileURLToPath(import.meta.url))
+        const pathToList = join(_dirname, 'files')
+
+        await access(pathToList)
+
+        const files = await readdir(pathToList)
+        console.log(files)
+
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed')
+        }
+    }
 };
 
 await list();
