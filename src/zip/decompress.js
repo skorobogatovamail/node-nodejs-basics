29a70c9dacab82cc4fb25978e5e745b2356a2ada
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-04-29 00:36:11 +0300

    zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const archiveFile = join(__dirname, 'files', 'archive.gz');
+    const outputFile = join(__dirname, 'files', 'fileToCompress.txt');
+
+    const readStream = createReadStream(archiveFile);
+    const writeStream = createWriteStream(outputFile);
+    const gunzip = createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
+
+    return new Promise((resolve, reject) => {
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+        readStream.on('error', reject);
+    });
 };
 
 await decompress();
